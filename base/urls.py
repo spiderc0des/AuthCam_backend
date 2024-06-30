d4d1d620277fd8b49096f4935e9ab105e0654b7a
spider <abdulrahmanyusuf2002@gmail.com>
2024-06-30 22:52:53 +0100

    added test page

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('upload/', view=post_media_info),
     path('verify/', view=get_media_info),
-    path('test/', views=test)
+    path('test/', view=test)
 
 ]
